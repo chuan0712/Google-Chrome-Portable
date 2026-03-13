e25c77adce509e3f8fd82dba8af862ac52906779
panghuhu <majiayouxiang2021@gmail.com>
2026-03-13 19:06:12 +0800

    优化 Chrome 更新和构建流程：添加错误处理，确保版本目录存在，重构文件移动和压缩步骤

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import os
 import shutil
 import xml.dom.minidom
 from datetime import datetime
-
 import requests
 
+# --- 1. 获取 Chrome 更新信息 ---
 url = 'https://tools.google.com/service/update2'
 
 # Create XML request message for Google Omaha
@@ -18,48 +18,72 @@ data = """<?xml version="1.0" encoding="UTF-8"?>
 	</app>
 </request>"""
 
-response = requests.post(url, data=data)
-
-dom = xml.dom.minidom.parseString(response.text)
-
-print(dom.toprettyxml(indent='  '))
-
-url = dom.getElementsByTagName("url")[0].getAttribute("codebase")
-name = dom.getElementsByTagName("action")[0].getAttribute("run")
-
-print(url, name)
-
-response = requests.get(url + name)
-
-with open("chrome.7z.exe", "wb") as file:
-    file.write(response.content)
-
-os.system('chmod +x ./7zzs')
-os.system('./7zzs x chrome.7z.exe')
-os.system('./7zzs x chrome.7z')
-
-# 获得Chrime-bin,version.dll,组装到一块就可以分发了
-version = '0.0.0.0'
-path = 'Chrome-bin'
-for i in os.listdir(path):
-    if os.path.isdir(os.path.join(path, i)):
-        version = i
-        break
-
-print(version)
-if version == '0.0.0.0':
+try:
+    response = requests.post(url, data=data)
+    dom = xml.dom.minidom.parseString(response.text)
+    url_base = dom.getElementsByTagName("url")[0].getAttribute("codebase")
+    exe_name = dom.getElementsByTagName("action")[0].getAttribute("run")
+except Exception as e:
+    print(f"Error fetching update info: {e}")
     exit(1)
 
-# 获得Chrome-bin,version.dll,组装到一块就可以分发了
-shutil.move(r'version.dll', 'Chrome-bin')
-shutil.move(r'chrome++.ini', 'Chrome-bin')
+# --- 2. 下载并解压 ---
+print(f"Downloading: {url_base}{exe_name}")
+res = requests.get(url_base + exe_name)
+with open("chrome.7z.exe", "wb") as f:
+    f.write(res.content)
 
-os.rename(r'Chrome-bin', 'Chrome')
-shutil.move(r'Chrome', 'build/release/Chrome')
+# 给 7zzs 权限并解压
+os.system('chmod +x ./7zzs')
+os.system('./7zzs x chrome.7z.exe -y')
+os.system('./7zzs x chrome.7z -y')
 
-# 会自动封装为zip
-env = os.getenv('GITHUB_ENV')
-with open(env, 'a') as f:
-    f.write(f'BUILD_NAME=Win64_{version}_{datetime.now().strftime("%Y-%m-%d")}')
+# --- 3. 确定版本号 ---
+version = '0.0.0.0'
+path = 'Chrome-bin'
+if os.path.exists(path):
+    for i in os.listdir(path):
+        if os.path.isdir(os.path.join(path, i)):
+            version = i
+            break
 
-# os.system(f'7z.exe a build/release/Win64_{version}_{datetime.now().strftime("%Y-%m-%d")}.7z Chrome')
+if version == '0.0.0.0':
+    print("Error: Could not find version directory.")
+    exit(1)
+
+# --- 4. 组装便携版目录 ---
+build_dir = 'build/release'
+if os.path.exists(build_dir):
+    shutil.rmtree(build_dir)
+os.makedirs(build_dir)
+
+# 移动 DLL 和配置文件到 Chrome-bin
+if os.path.exists('version.dll'):
+    shutil.move('version.dll', 'Chrome-bin')
+if os.path.exists('chrome++.ini'):
+    shutil.move('chrome++.ini', 'Chrome-bin')
+
+# 重命名为 Chrome 文件夹
+if os.path.exists('Chrome'):
+    shutil.rmtree('Chrome')
+os.rename('Chrome-bin', 'Chrome')
+
+# --- 5. 执行 7z 压缩 ---
+# 目标文件名固定为需求格式
+fixed_filename = "chrome-windows-amd64.7z"
+archive_path = os.path.join(build_dir, fixed_filename)
+
+print(f"Compressing Chrome to {archive_path}...")
+# -mx=9 极限压缩, -ms=on 固实压缩以获得更高压缩率
+os.system(f'./7zzs a -mx=9 -ms=on "{archive_path}" ./Chrome')
+
+# --- 6. 写入 GitHub 环境变量 ---
+env_file = os.getenv('GITHUB_ENV')
+if env_file:
+    with open(env_file, 'a') as f:
+        # 传递 Tag 名，例如 v145.0.7632.160
+        f.write(f'RELEASE_TAG=v{version}\n')
+        # 传递压缩包路径供上传使用
+        f.write(f'ARCHIVE_PATH={archive_path}\n')
+
+print(f"Done. Version: {version}")
